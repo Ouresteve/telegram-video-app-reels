a6575e3aee3e101d9d572e30446d0a3e05dd7432
Ouresteve <144885615+Ouresteve@users.noreply.github.com>
2025-05-02 20:21:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ function createProfilePage() {
     profileDiv.classList.add('profile-page');
 
     const profilePic = document.createElement('img');
-    profilePic.src = 'videos/profile.jpg';
+    profilePic.src = 'webapp/videos/profile.jpg';
     profilePic.alt = 'Profile Picture';
     profilePic.classList.add('profile-pic');
 
